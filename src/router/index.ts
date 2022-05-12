09c001f245eeeee6447794a9f71211322652a892
kim manjoong <akswnd98@gmail.com>
2022-05-12 10:30:31 +0900

    public path를 /api로 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,17 +6,21 @@ import winstonLogger from '@/src/winstonLogger';
 
 const app = express();
 
-app.get('/randomYoutubeIds', async (req, res: Response<GetRandomYoutubeIdsResponse>) => {
+const router = express.Router();
+
+router.get('/randomYoutubeIds', async (req, res: Response<GetRandomYoutubeIdsResponse>) => {
   try {
-    const rst = await NodeModel.findAll({ attributes: ['ytId'], order: sequelize.random(), limit: process.env.NUM_OF_IDS_PER_REQUEST });
+    const rst = await NodeModel.findAll({ attributes: ['ytId'], order: sequelize.random(), limit: Number(process.env.NUM_OF_IDS_PER_REQUEST) });
     res.send({
       ids: rst.map((v) => v.ytId!),
     });
     res.end();
   } catch (e) {
-
+    winstonLogger.error(e);
   }
 });
+
+app.use('/api', router);
 
 app.listen(process.env.SERVER_PORT, () => {
   winstonLogger.info(`started on port ${process.env.SERVER_PORT}`);
